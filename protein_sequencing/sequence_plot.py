f83e889bbe822a844425d3d5a081bf047c27e0d8
Tarik Alnawa <talnawa@outlook.de>
2024-05-08 11:17:31 +0200

    Add clustalw2 for alignment and first sequence plot

--- protein_sequencing/sequence_plot.py
+++ protein_sequencing/sequence_plot.py
@@ -2,24 +2,61 @@ import plotly.graph_objects as go
 import os
 import uniprot_align
 import parameters
+import numpy as np
 
 def create_plot(input_file: str | os.PathLike, output_path: str | os.PathLike) -> str:
     output_file = f"{output_path}_figure.png"
 
-    alignments = uniprot_align.align_protein(input_file)
+    alignments = list(uniprot_align.get_alignment(input_file))
+    max_length = 0
+    for alignment in alignments:
+        if len(alignment.seq) > max_length:
+            max_length = len(alignment.seq)
+        # print(alignment.seq)
 
-    layout = go.Layout(
-        font=dict(size=parameters.FONT_SIZE),
-        width = parameters.FIGURE_WIDTH,
-        height = parameters.FIGURE_HEIGHT
-    )
+    assert all(len(alignment.seq) == max_length for alignment in alignments)
 
-    fig = go.Figure(data = [], layout=layout)
-    
+    different_possibilities = [-1]*max_length
+    for i in range(len(alignments[0].seq)):
+        proteins = set()
+        for alignment in alignments:
+            protein = alignment.seq[i]
+            proteins.add(protein)
+        
+        if '-' in proteins:
+            if len(proteins) == 2:
+                different_possibilities[i] = -1
+            if len(proteins) > 2:
+                different_possibilities[i] = len(proteins)-1
+        else:
+            different_possibilities[i] = len(proteins)
+
+    print(max_length)
+    print(different_possibilities)
+
+    width = max_length
+    height = 50
+    rectangle = np.zeros((height, width))
+    for i, value in enumerate(different_possibilities):        
+        rectangle[:, i] = value
+
+    fig = go.Figure(data=go.Heatmap(z=rectangle))
 
     fig.show()
     fig.write_image(f'{output_path}/fig1.png')
 
+    clean_up()
+
     return output_file
+
+def clean_up():
+    directory = 'data/tmp'
+
+    files = os.listdir(directory)
+
+    for file_name in files:
+        file_path = os.path.join(directory, file_name)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 create_plot('data/uniprot_data/tau_isoforms2N4R.fasta', 'output')
--- protein_sequencing/uniprot_align.py
+++ protein_sequencing/uniprot_align.py
@@ -1,32 +1,18 @@
 from Bio import AlignIO, SeqIO, Seq, Align
-
 import os
+import subprocess
 
-
-def align_protein(input_file: str | os.PathLike):
-    # code from: https://stackoverflow.com/questions/32833230/biopython-alignio-valueerror-says-strings-must-be-same-length
-
+def get_alignment(input_file: str | os.PathLike):
     records = list(SeqIO.parse(input_file, 'fasta'))
 
-    # get max sequence length
-    max_length = max(len(record.seq) for record in records)
-
-    # pad sequences so that they all have the same length
-    for record in records:
-        if len(record.seq) < max_length:
-            sequence = str(record.seq).ljust(max_length, '.')
-            record.seq = Seq.Seq(sequence)
-
-    # check that all sequences have the same length         
-    assert all(len(record.seq) == max_length for record in records)
-
     # write to temporary file and do alignment
-    output_file = f'{os.path.splitext(input_file)[0]}_padded.fasta'
-    with open(output_file, 'w') as f:
+    padded_sequences_path = f'data/tmp/{os.path.splitext(os.path.basename(input_file))[0]}_padded'
+    with open(f"{padded_sequences_path}.fasta", 'w') as f:
         SeqIO.write(records, f, 'fasta')
 
-    alignment = AlignIO.parse(output_file, "fasta")
-    os.remove(output_file)
-
-    # alignment ready
-    return alignment
+    cmd = f"./clustalw-2.1-linux/clustalw2 -infile={padded_sequences_path}.fasta"
+    subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, text=True)
+    
+    align = AlignIO.read(f"{padded_sequences_path}.aln", "clustal")
+    
+    return align
