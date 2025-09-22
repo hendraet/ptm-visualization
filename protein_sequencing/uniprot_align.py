70390b107315d46f27c6f4e2b3d470e5351e1ba5
hendraet <hendrik.raetz@niederjahna.de>
2025-09-22 09:51:42 +0200

    Changes calling of clustalo binary

--- protein_sequencing/uniprot_align.py
+++ protein_sequencing/uniprot_align.py
@@ -27,9 +27,9 @@ def get_alignment(input_file: Path | str, out_dir: Path | str) -> AlignIO.Multip
     if len(records) == 1:
         align = records
     else:
-        # TODO: magic path for now, but this won't work with a proper library installation
-        #   - maybe do relative to __file__?
-        clustal_omega_path = '/home/hendraet/stud_sync/Studium/phd/proteomics/PROTzilla/backend/ptm-visualization/clustal-omega/clustalo-1.2.4-Ubuntu-x86_64'
+        # TODO: link to appropriate binary for OS during installation - would be cool to have them permanently attached
+        #  to the project, but since it's a couple MB big, maybe not as part of the repo (but git-lfs?)
+        clustal_omega_path = Path(__file__).parent.parent / 'clustal-omega' / 'clustalo'
         cmd = f"{clustal_omega_path} \
                 --infile={padded_sequences_path} \
                 --outfile={aligned_fasta_path} \
